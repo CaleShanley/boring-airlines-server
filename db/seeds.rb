1d660901a28ed97d08a7dae9053c46d708a53d4e
CaleShanleyy <CaleShanley1@outlook.com>
2020-07-07 18:56:03 +1000

    Fixed Models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+u = User.create :email => "joel@ga.com", :password => "chicken"
+Airplane.destroy_all
+a = Airplane.create :name => "747", :rows => "4", :columns => "24"
+Flight.destroy_all
+f = Flight.create :date => "2020-07-07", :flightnumber => "1231", :fromto => "BNE >> SYD", :plane => "22", :seats => "182"
+Reservation.destroy_all
+r = Reservation.create
+
+a.flights << f
+u.reservations << r
+u.reservations << r
+f.reservations << r
