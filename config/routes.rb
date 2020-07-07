d53889c43555d710dfdf0a3467d67f9f9f84b9b7
CaleShanleyy <CaleShanley1@outlook.com>
2020-07-07 17:45:30 +1000

    Added Models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :reservations
+  get 'users/new'
+  resources :airplanes
   resources :flights
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
